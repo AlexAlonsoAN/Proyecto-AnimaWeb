1f7364b5f7ea9334a6dac603c39b071004e47a7d
ErikaPlanchesteiner <erika.planchesteiner@anima.edu.uy>
2024-04-12 19:17:47 -0300

    .env.example
    Co-Authored-By: AlexAlonsoAN <148915156+AlexAlonsoAN@users.noreply.github.com>
    Co-Authored-By: Avrilolivera <148917592+Avrilolivera@users.noreply.github.com>
    Co-Authored-By: JOELdeRUGERIS <149019798+JOELdeRUGERIS@users.noreply.github.com>

--- sequelize-api-associations-project (1)/controllers/adminController.js
+++ sequelize-api-associations-project (1)/controllers/adminController.js
@@ -1,14 +1,27 @@
 const { Admin } = require("../models");
+const { findByPk } = require("../models/User");
 
 const adminController = {
-  index: async (req, res) => {},
-  show: async (req, res) => {},
+  index: async (req, res) => {
+    const admins = await Admin.findAll();
+    return res.json(admins);
+  },
+  show: async (req, res) => {
+    const { id } = req.params;
+    const admin = await admin.findByPk(id);
+    return res.jason(admin);
+  },
   store: async (req, res) => {
-    const { title, content, userId } = req.body;
-    await Admin.create({ title, content, userId });
+    const { firstname, lastname, email, password } = req.body;
+    await Admin.create({ firstname, lastname, email, password });
     return res.send("El admin fue creado con éxito!");
   },
-  update: async (req, res) => {},
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { firstname, lastname, email, password } = req.body;
+
+    
+  },
   destroy: async (req, res) => {},
 };
 
