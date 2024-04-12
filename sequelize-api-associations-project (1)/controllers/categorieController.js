ed4ff76eb000f386cc5037c93cbe8403488d7223
JOELdeRUGERIS <joel.derugeris@anima.edu.uy>
2024-04-12 20:49:14 -0300

    categorie/product-controller

--- sequelize-api-associations-project (1)/controllers/categorieController.js
+++ sequelize-api-associations-project (1)/controllers/categorieController.js
@@ -1,11 +1,39 @@
-const { categorie } = require("../models");
+const { categorie, Categorie } = require("../models");
 
 const categorieController = {
-  index: async (req, res) => {},
-  show: async (req, res) => {},
-  store: async (req, res) => {},
-  update: async (req, res) => {},
-  destroy: async (req, res) => {},
+  index: async (req, res) => {
+    const categorie = await Categorie.findAll();
+    return res.json(categorie);
+  },
+  show: async (req, res) => {
+    const { id } = req.params;
+    const categorie = await Categorie.findByPk(id);
+    return res.json(categorie);
+  },
+  store: async (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+    await Categorie.create({ firstname, lastname, email, phonenumber, password });
+    return res.send("El usuario fue creado con éxito!");
+  },
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { firstname, lastname, email, phonenumber, password } = req.body;
+
+    const categorie = await Categorie.findByPk(id);
+
+    if (firstname) Categorie.firstname = firstname;
+    if (lastname) Categorie.lastname = lastname;
+    if (email) Categorie.email = email;
+    if (password) Categorie.password = password;
+    if (phonenumber) Categorie.phonenumber = phonenumber;
+
+    await categorie.save();
+
+    return res.send("Usuario modificado con éxito!");
+  },
+  destroy: async (req, res) => {
+    router.get("/", async (req, res) => {});
+  },
 };
 
 module.exports = categorieController;
--- sequelize-api-associations-project (1)/controllers/productController.js
+++ sequelize-api-associations-project (1)/controllers/productController.js
@@ -1,11 +1,39 @@
-const { product } = require("../models");
+const { Product } = require("../models");
 
 const productController = {
-  index: async (req, res) => {},
-  show: async (req, res) => {},
-  store: async (req, res) => {},
-  update: async (req, res) => {},
-  destroy: async (req, res) => {},
+  index: async (req, res) => {
+    const product = await Product.findAll();
+    return res.json(product);
+  },
+  show: async (req, res) => {
+      const { id } = req.params;
+      const product = await Product.findByPk(id);
+      return res.json(product);
+  },
+  store: async (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+  await Product.create({ firstname, lastname, email, phonenumber, password });
+  return res.send("El usuario fue creado con éxito!");},
+  
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { firstname, lastname, email, phonenumber, password } = req.body;
+
+    const user = await Product.findByPk(id);
+
+    if (firstname) Product.firstname = firstname;
+    if (lastname) Product.lastname = lastname;
+    if (email) Product.email = email;
+    if (password) Product.password = password;
+    if (phonenumber) Product.phonenumber = phonenumber;
+
+    await product.save();
+
+    return res.send("Usuario modificado con éxito!");
+  },
+  destroy: async (req, res) => {
+    router.get("/", async (req, res) => {});
+  },
 };
 
 module.exports = productController;
