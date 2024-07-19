103969969fbb0de2bc7cc85a6adf11e4a7b3f04d
AlexAlonsoAN <148915156+AlexAlonsoAN@users.noreply.github.com>
2024-07-19 09:36:26 -0300

    upd project

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const cors = require("cors")
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 const port = process.env.APP_PORT;
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.listen(port, () =>
-  console.log(`Server running in http://${process.env.APP_DOMAIN}.\n`)
+  console.log(`Server running on http://${process.env.APP_DOMAIN}.\n`)
 );
+
+// TODO: escribir las variables de entorno.
